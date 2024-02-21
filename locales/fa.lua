93ee600ba9fc6980076bf5fde22e7e6393a393ed
Ali Louie <saliwesm835@gmail.com>
2024-02-21 13:35:14 +0330

    Persian Language

---
+++ locales/fa.lua
@@ -0,0 +1,30 @@
+Locales['fa'] = {
+    ['permissions'] = 'Shoma dastresi kafi baraye in dastur ra nadarid.',
+
+    ['allowlist_check'] = 'Dar hale barresi liste mojaz',
+    ['auto_allowlist'] = 'Dar hale ezafe kardan be liste mojaz',
+	['not_allowlisted'] = 'Shoma dar Liste mojaze shahr nistid!',
+
+    ['identifier_error'] = 'Dar hale hazer khatayei dar hoviyat ast, be dalile nadashtane %s:',
+    ['identifier_missing'] = 'Shoma hich id sabt nakardid',
+
+    ['identifier_length_error'] = 'There is a error in the identifier %sID\'s length, it needs a length of %s with out %s: submitted length was %s',
+    ['identifier_steam'] = '17 when using Steam64 and 15 when using Steam Hex!',
+
+    ['allowlist_added'] = 'Shoma %s be liste mojaz ezafe shodid',
+    ['allowlist_added_with_priority'] = 'Shoma %s be liste mojaz ezafe shodid ba Olaviyate %s',
+    ['allowlist_removed'] = 'Shoma az liste mojaz %s hazf shodid',
+
+    ['priority_numeric'] = 'Olaviyat bayad adaddi bashad!',
+    ['priority_updated'] = 'You updated %s priority to %s',
+
+    ['allowlist_found'] = 'You tried to add %s to the allowlist but the id is already on the allowlist',
+    ['allowlist_not_found'] = 'You tried to rmove %s from the allowlist but the id is not in the allowlist',
+    ['allowlist_not_found_priority'] = 'You tried to change priority on %s but the id is not found in the allowlist',
+
+    ['allowlist_loaded_with_priority'] = 'Loaded %s identifiers and added %s to the priority list',
+    ['allowlist_loaded'] = 'Loaded %s identifiers',
+
+    ['reloaded'] = 'Liste Mojaz Bargozari shod.',
+    ['auto_allowlist_warning'] = 'Warning first person that connects to the server will be added to the allowlist automatic',
+}
